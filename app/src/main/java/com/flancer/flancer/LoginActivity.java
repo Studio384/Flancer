024972efdb756e11183a13c7af42794b69c082f2
arnozeelmaekers <arnozeel@gmail.com>
2017-10-10 11:49:46 +0200

    add login function

--- app/src/main/java/com/flancer/flancer/LoginActivity.java
+++ app/src/main/java/com/flancer/flancer/LoginActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -12,8 +13,21 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
     }
-    public void login(View v){
-        Intent intent = new Intent(this,JobListActivity.class);
-        startActivity(intent);
+    public void login(){
+        EditText editPassField = (EditText)findViewById(R.id.password);
+        String passField = editPassField.getText().toString();
+        EditText editLoginField = (EditText)findViewById(R.id.email);
+        String loginField = editLoginField.getText().toString();
+
+        if(passField.equals("test") && loginField.equals("test")) {
+            Intent intent = new Intent(this, JobListActivity.class);
+            startActivity(intent);
+        }
+    }
+    public void creatCompany(){
+
+    }
+    public void creatUser(){
+
     }
 }
